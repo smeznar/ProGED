8830cd55864d2f2e120ed82a2f6bb6228b2d7770
bostjangec <bostogec@gmail.com>
2020-11-11 19:40:59 +0100

    Stranger things ... coverage_external makes mistakes, can calculate height 10*100 and make difference when dict[(A,h)] vs. dict[(str(A),h)]

--- generators/grammar_archive.py
+++ generators/grammar_archive.py
@@ -26,7 +26,8 @@ class GeneratorGrammar (BaseExpressionGenerator):
                              "Input: " + str(grammar))
                 
         self.start_symbol = self.grammar.start()
-    
+        self.coverage_dict = {}
+
     def generate_one (self):
         return generate_sample(self.grammar, items=[self.start_symbol])
     
@@ -107,12 +108,69 @@ class GeneratorGrammar (BaseExpressionGenerator):
                 else:
                     if (symbol, height-1) in self.coverage_dict:
                         subprobabs *= self.coverage_dict[(symbol, height-1)]
+                        # subprobabs *= self.coverage_dict[(str(symbol), height-1)]
                     else:
-                        newprob = self.count_coverage_fast(symbol, height-1)
+                        newprob = self.count_coverage_external(symbol, height-1)
+                        # newprob = self.count_coverage_external(str(symbol), height-1)
                         self.coverage_dict[(symbol, height)] = newprob
+                        # self.coverage_dict[(str(symbol), height)] = newprob
                         subprobabs *= newprob
             coverage += subprobabs
         return coverage
+
+    def list_coverages(self, height, tol=10**(-17),
+                            min_height=100, verbosity=0):
+        """Counts coverage of maximal height using cache(dictionary).
+        Input:
+            height - maximal height of parse trees of which the
+                coverage is calculated of.
+            tol - tolerance as a stopping condition. If change
+                is smaller than the input tolerance, then it stops.
+            min_height - overrides tolerance stopping condition and
+                calculates coverage of all heights <= min_height. It
+                also determines for how many previous steps the change
+                is measured, i.e. for levels (height-1 - min_height/2).
+            verbosity - if set to > 0, it prints stopping probability
+                change, height and input tolerance.
+        Output:
+            Dictionary with nonterminals as keys and coverages of all
+            parse trees with root in given key nonterminal and their
+            heights at most the input height as their values.
+        """
+        nonterminals = list(set([prod.lhs() for prod
+                                in self.grammar.productions()]))
+        if height == 0:
+            return {A: 0 for A in nonterminals}
+        probs_dict = {}
+        for A in nonterminals:      # height = 0:
+            probs_dict[(A, 0)] = 0
+        min_height = max(min_height, 2)  # to avoid int(min_height/2)=0
+        for level in range(1, height+1):    # height > 0:
+            if level > min_height:  # Do `min_height` levels without stopping.
+                # Measure change from last min_height/2 levels:
+                change = max(abs(probs_dict[(A, level-1)]
+                                 - probs_dict[(A, level-int(min_height/2))])
+                                 for A in nonterminals)
+                if change < tol:
+                    if verbosity > 0:
+                        print(change, level, tol, "change of probability")
+                    return {A: probs_dict[(A, level-1)] for A in nonterminals}
+            for A in nonterminals:
+                coverage = 0
+                prods = self.grammar.productions(lhs=A)
+                for prod in prods:
+                    subprobabs = prod.prob()
+                    for symbol in prod.rhs():
+                        if not isinstance(symbol, Nonterminal):
+                            continue  # or subprobabs = 1
+                        else:
+                            subprobabs *= probs_dict[(symbol, level-1)]
+                    coverage += subprobabs
+                probs_dict[(A, level)] = coverage
+        if verbosity > 0:
+            print("The input height %d was reached. " % height
+                +"Bigger height is needed for better precision.")
+        return {A: probs_dict[(A, height)] for A in nonterminals}
 
     def enumerate_trees(self, start, height):
         """Enumerates all parse trees of height <= height."""
@@ -165,8 +223,6 @@ class GeneratorGrammar (BaseExpressionGenerator):
 
 
         return None
-
-
 
     def __str__ (self):
         return str(self.grammar)
@@ -305,7 +361,6 @@ pgram1 = GeneratorGrammar("""
     C -> 'c' [1]
     D -> 'd' [1]
 """)
-
 pgramSS = GeneratorGrammar("""
     S -> S S [0.3]
     S -> 'a' [0.7]  
@@ -315,12 +370,7 @@ def pgramSSparam(p=0.3):
             S -> S S [{p}]
             S -> 'a' [{1-p}]  
 """)
-
-# pgramSSc = pgramSSparam(0.6)
-# print(pgramSSc)
-# print(pgramSSc.count_coverage(pgramSSc.start_symbol,17), "coverage")
-
-grama = GeneratorGrammar("""
+pgrama = GeneratorGrammar("""
     S -> A B [0.7]
     S -> 'a' [0.1]
     S -> 'b' [0.1]
@@ -338,16 +388,34 @@ grama = GeneratorGrammar("""
     A12 -> 'af' [0.3]
     A12 -> 'bf' [0.7]
 """)
+pgramw = GeneratorGrammar("""
+S -> A B A2 A1 B2 B1 [0.7]
+A -> A1 A2 [0.3]
+B -> B1 B2 [0.1]
+S -> 'a' [0.1]
+S -> 'b' [0.1]
+S -> 'c' [0.1]
+A -> 'aq' [0.5]
+A -> 'bq' [0.2]
+B -> 'aw' [0.1]
+B -> 'bw' [0.8]
+A2 -> 'ak' [0.4]
+A2 -> 'bk' [0.6]
+A1 -> 'ar' [0.8]
+A1 -> 'br' [0.2]
+B2 -> 'af' [1]
+B1 -> 'bf' [1]
+""")
 
 ### testing:
-gram = GeneratorGrammar("E -> 'x' [0.7] | E '*' 'x' [0.3]")
+grammar = GeneratorGrammar("E -> 'x' [0.7] | E '*' 'x' [0.3]")
 # for gramm in [gram, grama, pgramSS, pgram1, pgram0]:
 #     print(f"For grammar:\n {gramm}")
 #     for i in [10]:
 #         print(gramm.count_trees(gramm.start_symbol,i), f" = count trees of height <= {i}")
 #         print(gramm.count_coverage(gramm.start_symbol,i), f" = total probability of height <= {i}")
 
-p=0.6
+# p=0.6
 # for gramm in [pgramSSparam(p)]:
 #     print(f"For grammar:\n {gramm}")
 #     for i in range(0,20):
@@ -357,9 +425,39 @@ p=0.6
 # p=0.8:
 # 0.2499 = 0.25  
 # 1/p -1 = 1/0.8 - 1 = 0.25
-i=4
-trees = gram.enumerate_trees(gram.start_symbol,i)
-print(f" all trees of height <= {i}:")
-[print(tree) for tree in trees]
-print("Test results for count_coverage_for() : for height 10**5, "+
-        "the grammar S->SS needs half of a second.")
+# i=4
+# trees = gram.enumerate_trees(gram.start_symbol,i)
+# print(f" all trees of height <= {i}:")
+# [print(tree) for tree in trees]
+# print("Test results for count_coverage_for() : for height 10**5, "+
+        # "the grammar S->SS needs half of a second.")
+# gramm = pgramSS
+# for i in range(15,100000):
+#     # print(gramm.count_coverage(gramm.start_symbol,i), f" = coverage(start,{i}) of height <= {i}")
+#     print(gramm.count_coverage_external(gramm.start_symbol,i), f" = coverage(start,{i}) of height <= {i}")
+
+
+from time import time
+t1=0
+def display_time(t1): t2 = time(); print(10**(-3)*int((t2-t1)*10**3)); return t2
+# height = 10**100
+height = 10**4
+p=0.9
+for gramm in [grammar, pgram0, pgram1, pgrama, pgramw, pgramSS, 
+                pgramSSparam(p) ]:
+    print(f"\nFor grammar:\n {gramm}")
+    # for i in range(height, height+1):
+    for i in range(0, 10):
+        # t2=display_time(t1); t1=t2
+        # # print(gramm.count_trees(gramm.start_symbol,i), f" = count trees of height <= {i}")
+        # print(gramm.count_coverage(gramm.start_symbol,i), f" = coverage(start,{i}) of height <= {i}")
+        t2=display_time(t1); t1=t2
+        gramm.coverage_dict = {}
+        print(gramm.count_coverage_external(gramm.start_symbol,i), f" = coverage_external(start,{i}) of height <= {i}")
+        t2=display_time(t1); t1=t2;
+        print(gramm.list_coverages(i, tol=10**(-17), min_height=height,
+            verbosity=0)[gramm.grammar.start()], 
+            f" = list_coverages({i})[start] of height <= {i}")
+        t2=display_time(t1); t1=t2
+    # print("\nRenormalized grammar:\n %s" % gramm.renormalize())
+print(f"Chi says: limit probablity = 1/p - 1, i.e. p={p} => prob={1/p-1}")
