106c13c7cb87b27252c2f86f1b27b14e08e0d03b
bostjangec <bostogec@gmail.com>
2021-02-10 07:36:52 +0100

    fibonacci.py example direct

--- ProGED/examples/oeis/fibonacci.py
+++ ProGED/examples/oeis/fibonacci.py
@@ -18,12 +18,12 @@ oeis = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,
  121393,196418,317811,514229,832040,1346269,
  2178309,3524578,5702887,9227465,14930352,24157817,
  39088169,63245986,102334155]
-oeis_primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,
- 61,67,71,73,79,83,89,97,101,103,107,109,113,127,
- 131,137,139,149,151,157,163,167,173,179,181,191,
- 193,197,199,211,223,227,229,233,239,241,251,257,
- 263,269,271][:40+1]
-oeis = oeis_primes
+# oeis_primes = [2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,
+#  61,67,71,73,79,83,89,97,101,103,107,109,113,127,
+#  131,137,139,149,151,157,163,167,173,179,181,191,
+#  193,197,199,211,223,227,229,233,239,241,251,257,
+#  263,269,271][:40+1]
+# oeis = oeis_primes
 fibs = np.array(oeis).reshape(-1, 1)
 ts = np.array([i for i in range(40+1)]).reshape(-1, 1)
 # print(ts, type(fibs), fibs.shape, type(ts[0]), ts.shape)
@@ -49,12 +49,12 @@ ED = EqDisco(data = data,
             task = None,
             target_variable_index = -1,
             variable_names=["n", "an"],
-            sample_size = 50,
-            verbosity = 1,
+            sample_size = 10,
+            verbosity = 0,
             generator = "grammar", 
             generator_template_name = "polynomial",
             generator_settings={"variables":["'n'"]},
-            estimation_settings={"verbosity": 1, "task_type": "algebraic", "lower_upper_bounds": 
+            estimation_settings={"verbosity": 0, "task_type": "algebraic", "lower_upper_bounds": 
             (-5, 5)} # meja, ko se najde priblizno: (-10,8)}# , "timeout": np.inf}
             #(0,1)}# , "timeout": np.inf}
             )
@@ -67,7 +67,7 @@ ED.fit_models()
 print(ED.models)
 # print(ED.get_results())
 # print(ED.get_stats())
-print("\n", ED.models, "\n\nFinal score:")
+print("\n\nFinal score:")
 for m in ED.models:
     print(f"model: {str(m.get_full_expr()):<30}; error: {m.get_error():<15}")
     
@@ -80,8 +80,8 @@ print(f"m  c0: {c0}", f"c1:{c1}")
 #         = round(phi**n/5**(1/2))
 #         = floor(phi**n/5**(1/2) + 1/2)
 
-# model = ED.models[5] 
-model = ED.models[15] 
+model = ED.models[5] 
+# model = ED.models[15] 
 print(model, model.get_full_expr(), model.get_error())
 res = model.evaluate(ts, *model.params)
 res = [int(np.round(flo)) for flo in res]
