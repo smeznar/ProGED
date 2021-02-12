b7d13ec56348bd5e99cb9e93ceab95c0928893fd
bostjangec <bostogec@gmail.com>
2021-02-12 21:19:24 +0100

    fibonaci recursive something

--- ProGED/examples/oeis/fibonacci_recursive.py
+++ ProGED/examples/oeis/fibonacci_recursive.py
@@ -1,23 +1,13 @@
 import numpy as np
-# import sympy as sp
-# # pg = ProGED
-# import ProGED as pg
-# # from pg.generate import generate_models
-# # gft = pg.generators.grammar_from_template
-# # from ProGED import generators.generate_models
 from ProGED.equation_discoverer import EqDisco
-# # from ProGED.generators.grammar import GeneratorGrammar
-# from ProGED.generators.grammar_construction import grammar_from_template
-# from ProGED.generate import generate_models
-# # from ProGED.model import Model
-# # from ProGED.model_box import ModelBox
-# # from ProGED.parameter_estimation import fit_models
+# from model.py import 
 
 oeis = [0,1,1,2,3,5,8,13,21,34,55,89,144,233,377,610,987,
  1597,2584,4181,6765,10946,17711,28657,46368,75025,
  121393,196418,317811,514229,832040,1346269,
  2178309,3524578,5702887,9227465,14930352,24157817,
  39088169,63245986,102334155]
+# fibs = np.array(oeis)
 fibs = np.array(oeis)
 # ts = np.array([i for i in range(40+1)]).reshape(-1, 1)
 # ts = np.array([i for i in range(40+1)])
@@ -37,25 +27,24 @@ fibs = np.array(oeis)
 # def f(i,j):
 #    return i+j 
 # template = np.fromfunction((lambda i,j:i+j), (40+1-2,2+1), dtype=int)
-
-def mdata(m, fibs):
-    """m -> (n-m) x (0/1+1+m) data matrix
-    m ... number of previous elements in formula
+# an = a{n-1} + a{n-2} is recurrece relation of order 2 (2 preceeding terms).
+def mdata(order, fibs):
+    """order -> (n-order) x (0/1+order) data matrix
+    order ... number of previous terms in formula
     """
     n = fibs.shape[0] # (40+1)
     # if ts.shape != fibs.shape:
     #     print("ts and fibs differend dimensions !!!!!")
     #     return 1/0
     # np.hstack(ts[:-m].reshape(-1,1), fibs[:-m])
-    indexes = np.fromfunction((lambda i,j:i+j), (n-m, m+1), dtype=int)
+    indexes = np.fromfunction((lambda i,j:i+j), (n-order, order+1), dtype=int)
     # first_column = indexes[:, [0]]
     # return np.hstack((first_column, fibs[indexes]))
     return fibs[indexes]
 # print(fibs)
 # print(mdata(2, fibs))
-data = mdata(2, fibs)
-
-
+order = 2
+data = mdata(order, fibs)
 
 
 # # variables = ["'n'"]
@@ -76,7 +65,7 @@ ED = EqDisco(data = data,
             task = None,
             target_variable_index = -1,
             variable_names=["an_2", "an_1", "an"],
-            sample_size = 16,
+            sample_size = 2,
             verbosity = 0,
             generator = "grammar", 
             generator_template_name = "polynomial",
@@ -86,11 +75,11 @@ ED = EqDisco(data = data,
             )
 # # print(data, data.shape)
 ED.generate_models()
+print(ED.models)
 ED.fit_models()
 # # try:
 # #     print(12/0)
 
-print(ED.models)
 # # print(ED.get_results())
 # # print(ED.get_stats())
 # print("\nFinal score:")
@@ -108,6 +97,19 @@ print(ED.models)
 
 # # model = ED.models[5] 
 model = ED.models[-1] 
+print(model.params, [int(p) for p in model.params])
+# :
+# an = model.lambdify([3])
+an = model.lambdify([int(p) for p in model.params])
+# print(an.)
+print(an, an(1,2))
+cache = list(fibs[:order])
+for i in range(len(fibs)):
+    cache += [an(*(cache[-order:]))]
+    # cache += [1]
+print(an(*cache[-order:]))
+print(cache)
+
 # res = model.evaluate(ts, *model.params)
 # res = [int(np.round(flo)) for flo in res]
 
