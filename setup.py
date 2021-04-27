9288ffc2cc3ecec4a76b7de6030d16592a3407b6
bostjangec <bostogec@gmail.com>
2021-04-27 10:59:37 +0200

    does it needs scikit-learn

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ setuptools.setup(
                         "scipy", 
                         "sympy", 
                         "nltk",
-                        "scikit-learn",
+                        # "scikit-learn",
                         "hyperopt",
                         "pytest",
                        ]
