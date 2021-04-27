2b5bec125f7e072396547220eb924608a279c433
bostjangec <bostogec@gmail.com>
2021-04-27 18:41:37 +0200

    new login script

--- ProGED/examples/oeis/cluster/login.sh
+++ ProGED/examples/oeis/cluster/login.sh
@@ -1,29 +1,42 @@
 #!/bin/bash
 # This is script for login node.
 
-pwd
-ls
-cd ProGED
-pwd
-ls
-cd ..
 git clone --branch ode https://github.com/brencej/ProGED
-echo "I (cluster) started to run .sh, first checking the imports: python3 import proged,numpy, itd."
-# singularity exec python-proged.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
-# singularity exec pyProGED.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
-singularity exec ~/pyProGED.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
+cp ProGED/ProGED/examples/oeis/fibonacci_oneby1.py .
+cp ProGED/ProGED/examples/oeis/oeis_selection.csv .
+cp ProGED/ProGED/examples/oeis/cluster/play.sh .
+cp ProGED/ProGED/examples/oeis/cluster/submit.xrsl .
+cp ProGED/tests/test_core.py .
 
-echo "I (cluster) am testing proged with pytest ..."
-# singularity exec python-proged.simg python3 -m pytest ProGED/tests/
-singularity exec pyProGED.simg python3 -m pytest clusterProGED/tests/
-# singularity exec ~/pyproged python3 -m pytest ../../../tests/test_core.py
-# singularity exec ~/pyProGED.simg python3 -m pytest clusterProGED/tests/test_core.py
-# singularity exec ~/pyProGED.simg python3 -m pytest ../../../tests/test_core.py
-echo "Let's finally execute the big program: "
-# First copy oeis data to current dir to avoid (no file) error.
-cp clusterProGED/ProGED/examples/oeis/oeis_selection.csv .
-singularity exec pyProGED.simg python3 clusterProGED/ProGED/examples/oeis/fibonacci_oneby1.py
-# singularity exec pyProGED.simg python3 fibonacci_oneby1.py  # online
-# singularity exec ~/pyProGED.simg python3 fibonacci_oneby1.py  # offline
-echo "... This is the end of your personal cluster script. Everything was executed successfully!"
+
+# play.sh:
+
+
+##!/bin/bash
+## Self replicating machine:
+#echo "first show current dir aka. pwd"
+#thisfile="~/play.sh"
+#echo " "
+#echo "I am the script called:"$thisfile
+#echo " "
+#echo " --- first line of "$thisfile" below : --- "
+#cat $thisfile
+#echo " --- this was the last line of "$thisfile" above.  --- "
+#echo " "
+
+#echo "one more try for git clone, ignore this"
+#git clone --branch ode https://github.com/brencej/ProGED clusterProGED/
+#ls 
+#echo "now for real:"
+
+#echo "I (cluster) started to run .sh, first checking the imports: python3 import proged,numpy, itd."
+#singularity exec ~/pyProGED.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
+
+#echo "I (cluster) am testing proged with pytest ..."
+#singularity exec ~/pyProGED.simg python3 -m pytest ~/test_core.py
+
+#echo "Let's finally execute the big program: "
+## First copy oeis data to current dir to avoid (no file) error.
+#singularity exec ~/pyProGED.simg python3 ~/fibonacci_oneby1.py  # online
+#echo "... This is the end of your personal cluster script. Everything was executed successfully!"
 
--- ProGED/examples/oeis/cluster/play.sh
+++ ProGED/examples/oeis/cluster/play.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
-# tar -xf ProGED.tar
+# This is script for login node.
+
+# git clone --branch ode https://github.com/brencej/ProGED
+# cp ProGED/ProGED/examples/oeis/fibonacci_oneby1.py .
+# cp ProGED/ProGED/examples/oeis/oeis_selection.csv .
+# cp ProGED/ProGED/examples/oeis/cluster/play.sh .
+# cp ProGED/ProGED/examples/oeis/cluster/submit.xrsl .
+# cp ProGED/tests/test_core.py .
+
+
+# play.sh:
+
+
+#!/bin/bash
 # Self replicating machine:
 echo "first show current dir aka. pwd"
-pwd
-thisfile="cluster/play.sh"
+thisfile="~/play.sh"
 echo " "
 echo "I am the script called:"$thisfile
 echo " "
@@ -11,23 +23,20 @@ echo " --- first line of "$thisfile" below : --- "
 cat $thisfile
 echo " --- this was the last line of "$thisfile" above.  --- "
 echo " "
+
+echo "one more try for git clone, ignore this"
 git clone --branch ode https://github.com/brencej/ProGED clusterProGED/
+ls 
+echo "now for real:"
+
 echo "I (cluster) started to run .sh, first checking the imports: python3 import proged,numpy, itd."
-# singularity exec python-proged.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
-# singularity exec pyProGED.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
 singularity exec ~/pyProGED.simg python3 -c "import ProGED, numpy, pandas, scipy, sympy, nltk, hyperopt, sklearn, pytest;print('Importing seems to work.')"
 
 echo "I (cluster) am testing proged with pytest ..."
-# singularity exec python-proged.simg python3 -m pytest ProGED/tests/
-singularity exec pyProGED.simg python3 -m pytest clusterProGED/tests/
-# singularity exec ~/pyproged python3 -m pytest ../../../tests/test_core.py
-# singularity exec ~/pyProGED.simg python3 -m pytest clusterProGED/tests/test_core.py
-# singularity exec ~/pyProGED.simg python3 -m pytest ../../../tests/test_core.py
+singularity exec ~/pyProGED.simg python3 -m pytest ~/test_core.py
+
 echo "Let's finally execute the big program: "
 # First copy oeis data to current dir to avoid (no file) error.
-cp clusterProGED/ProGED/examples/oeis/oeis_selection.csv .
-singularity exec pyProGED.simg python3 clusterProGED/ProGED/examples/oeis/fibonacci_oneby1.py
-# singularity exec pyProGED.simg python3 fibonacci_oneby1.py  # online
-# singularity exec ~/pyProGED.simg python3 fibonacci_oneby1.py  # offline
+singularity exec ~/pyProGED.simg python3 ~/fibonacci_oneby1.py  # online
 echo "... This is the end of your personal cluster script. Everything was executed successfully!"
 
