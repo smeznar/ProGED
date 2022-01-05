9e5abba95eafab68d310947e487c7ae5b46b5656
bostjangec <bostogec@gmail.com>
2022-01-05 15:15:11 +0100

    new dataset

--- ProGED/examples/oeis/scraping/create_csv.py
+++ ProGED/examples/oeis/scraping/create_csv.py
@@ -14,42 +14,39 @@ from saved_new_bfile10000 import bseqs
 # bseqs.pop('A000043')
 # # bseqs.pop('A000043', None)
 # # 3.) select small:
-# from selection import select_small
 from selection import select
-small = select(bseqs, head_length=1000, has_titles=1, float_limit=float_limit)
-# # filter accordingly:
-# seqs = select_small(bseqs, has_titles=1)
+seqs = select(bseqs, head_length=1000, has_titles=1, float_limit=1e16)
 
 # # Then header, body. 
-# df = pd.DataFrame(seqs)
-# df_sorted = df.sort_index(axis=1)
-# print(df_sorted.head())
-csv_filename = "oeis_selection.csv"  # Masters
+df = pd.DataFrame(seqs)
+df_sorted = df.sort_index(axis=1)
+print(df_sorted.head())
+# csv_filename = "oeis_selection.csv"  # Masters
 csv_filename = "oeis_dmkd.csv"
-# # df_sorted.to_csv(csv_filename, index=False)
+df_sorted.to_csv(csv_filename, index=False)
 
 # # after download:
 check = pd.read_csv(csv_filename)
 print("Read file from csv:")
 print(check)
-
+# 1/0
 
 selection = (
-        "A000009", 
+        # "A000009", 
         "A000040", 
-        "A000045", 
+        # "A000045", 
         "A000124", 
-        "A000108", 
-        "A000219", 
+        # "A000108", 
+        # "A000219", 
         "A000292", 
         "A000720", 
-        "A001045", 
+        # "A001045", 
         "A001097", 
         "A001481", 
         "A001615", 
-        "A002572", 
-        "A005230", 
-        "A027642", 
+        # "A002572", 
+        # "A005230", 
+        # "A027642", 
         )
 
 for i in selection:
